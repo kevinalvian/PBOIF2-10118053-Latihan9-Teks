bae2dd01359424a7b2ca87d61082a9cf4dc098da
kevinalvian <55798459+kevinalvian@users.noreply.github.com>
2019-09-26 08:21:08 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118053/latihan9/teks/PBOIF210118053Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118053.latihan9.teks;
+
+/**
+ *
+ * @author
+ * NAMA : Kevin Alvian
+ * KELAS : IF2
+ * NIM : 10118053
+ */
+public class PBOIF210118053Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
